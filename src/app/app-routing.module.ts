a604f77fa48c0bf9b69cbcab40131e7a72e04fc0
Joseph Joel Quispe Alvarez <JosephQA3131@gmail.com>
2026-01-29 12:59:41 +0100

    Examen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VecinoListComponent } from './Components/vecino-list/vecino-list.component';
+import { JuntaComponent } from './Components/junta/junta.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"vecinoList",component:VecinoListComponent},
+  {path:"junta",component:JuntaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VecinoListComponent } from './Components/vecino-list/vecino-list.component';
+import { JuntaComponent } from './Components/junta/junta.component';
+import { FormsModule } from '@angular/forms';
+import { DGritarDirective } from './Directives/d-gritar.directive';
+import { PMayusculasPipe } from './Pipes/p-mayusculas.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VecinoListComponent,
+    JuntaComponent,
+    DGritarDirective,
+    PMayusculasPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
